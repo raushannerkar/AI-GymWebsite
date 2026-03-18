d5b9973b1d105e7fa55b0cc4938539444a9110d4
Raushan Nerkar <106027530+raushannerkar@users.noreply.github.com>
2026-03-18 15:44:03 +0000

    Change email credentials in configuration
    Updated email configuration with new credentials.

--- website_folder/__init__.py
+++ website_folder/__init__.py
@@ -17,8 +17,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'SMTP.GMAIL.COM'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'raushnerprojects@gmail.com'
-app.config['MAIL_PASSWORD'] = 'lpmm ohhv cwxm rzma'
+app.config['MAIL_USERNAME'] = 'rauss@gmail.com'
+app.config['MAIL_PASSWORD'] = 'xxxx xxxx xxxx xxxx'
 
 mail = Mail(app)
 
